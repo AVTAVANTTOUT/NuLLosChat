ed470e2176b2732b42c2c0cbc9f641ad0d7b4e00
AVTAVANTTOUT <elias.bouzekouk@ecoles-epsi.net>
2024-12-24 17:40:40 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,14 @@
-const CACHE_NAME = "messagerie-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/manifest.json",
-  "/assets/icons/icon-192x192.png",
-  "/assets/icons/icon-512x512.png"
-];
+const CACHE_NAME = "nulloschat-cache-v1";
+const urlsToCache = ["/", "/index.html", "/css/style.css", "/js/script.js"];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
